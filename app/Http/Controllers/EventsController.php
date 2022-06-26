f67742d38dadd5da56a1bb7270c2c39a9f190dea
Andrey <work.a.andrey@gmail.com>
2022-06-26 07:49:19 +0500

    Complete the getEventsWithWorkshops function that returns all events with workshops

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -101,7 +101,9 @@ class EventsController extends BaseController
      */
 
     public function getEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 1');
+        $events = Event::query()->with('workshops')->get();
+
+        return response()->json($events);
     }
 
 
